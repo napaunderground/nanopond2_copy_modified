0484c309cdfc328f96fb441e1fa11447be3f34e2
Barry Rountree <rountree@llnl.gov>
2016-02-02 07:39:09 -0800

    Broke out license into separate file.

--- nanopond-2.0.c
+++ nanopond-2.0.c
@@ -1,24 +1,3 @@
-/* *********************************************************************** */
-/*                                                                         */
-/* Nanopond version 1.9 -- A teeny tiny artificial life virtual machine    */
-/* Copyright (C) 2005 Adam Ierymenko - http://www.greythumb.com/people/api */
-/*                                                                         */
-/* This program is free software; you can redistribute it and/or modify    */
-/* it under the terms of the GNU General Public License as published by    */
-/* the Free Software Foundation; either version 2 of the License, or       */
-/* (at your option) any later version.                                     */
-/*                                                                         */
-/* This program is distributed in the hope that it will be useful,         */
-/* but WITHOUT ANY WARRANTY; without even the implied warranty of          */
-/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           */
-/* GNU General Public License for more details.                            */
-/*                                                                         */
-/* You should have received a copy of the GNU General Public License       */
-/* along with this program; if not, write to the Free Software             */
-/* Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110 USA     */
-/*                                                                         */
-/* *********************************************************************** */
-
 /*
  * Nanopond is just what it says: a very very small and simple artificial
  * life virtual machine.
